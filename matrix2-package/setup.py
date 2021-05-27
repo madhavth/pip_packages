394d1d39dc62e312d588c2e7af661b696ff47943
madhavth <madhav.thapa@yipl.com.np>
2021-05-27 10:21:27 +0545

    added a lot of sht

---
+++ matrix2-package/setup.py
@@ -0,0 +1,12 @@
+#setup.py
+from setuptools import setup
+
+setup(
+    name='matrix2',
+    scripts=['matrix2'],
+    version= '0.3',
+    description = 'simple project, simple life',
+	long_description = file: README.md,
+    author = 'madhavth'
+)
+
