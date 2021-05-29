4d4cdea89faf9acef88e1167954f14233ab03a41
madhavth <madhav.thapa@yipl.com.np>
2021-05-29 17:20:16 +0545

    version change from file

--- shellpackager-package/setup.py
+++ shellpackager-package/setup.py
@@ -1,10 +1,25 @@
 #setup.py
 from setuptools import setup
+import re
+
+version_read = re.search(
+    '^version\s*=\s*"(.*)"',
+    open('shellpackager').read(),
+    re.M
+)
+
+
+if version_read is not None:
+    version = version_read.group(1)
+else:
+    version = "0.1"
+
+print("version is " + version)
 
 setup(
     name='shellpackager',
     scripts=['shellpackager'],
-    version= '1.0.6.1',
+    version= version,
     description = 'simple project, simple life',
     long_description = 'cli tool to create pypi project from shell scripts',
       install_requires=[
